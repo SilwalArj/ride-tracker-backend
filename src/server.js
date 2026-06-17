afe1ffca09a893bf608dd58f86c5c83db117c7a0
SilwalArj <arjsilwal076@gmail.com>
2026-06-17 22:45:55 +0545

    fixed_socket backend

--- src/server.js
+++ src/server.js
@@ -69,6 +69,12 @@ io.on("connection", (socket) => {
   // LIVE LOCATION
   socket.on("sendLocation", (data) => {
     socket.to(data.groupCode).emit("receiveLocation", data);
+    console.log(
+      "LOCATION",
+      data.userName,
+      data.lat,
+      data.lng
+    );
   });
 
   // SOS
